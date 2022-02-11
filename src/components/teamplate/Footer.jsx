a27192d5f2f4468df3166f6e92fd189f6aebf610
IvanMatt8 <ivanmatt1995@gmail.com>
2022-02-11 10:52:23 -0300

    :sparkles: add Footer

---
+++ src/components/teamplate/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css"
+import React from "react"
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props => 
+     
+<footer className="footer">
+   <span>
+      Desenvolvido por
+      <strong> Ivan <span className="text-danger">M</span>att</strong>
+   </span>
+</footer>
